f2551665eed1fe88bbe23fbec37fcee142df2528
Matthew Lewis <Loneeagle14@aol.com>
2014-10-07 20:57:31 -0500

    Week 2 initial commit/setup
    -FINALLY got Xcode to cooperate after the old installation was
    corrupted somehow.  New version refused to recognize old Full Sail
    provisioning.
    -Created initial project files and the layout for the login activity

---
+++ Week 2/EventBook/EventBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EventBook
+//
+//  Created by Matthew Lewis on 10/6/14.
+//  Copyright (c) 2014 com.fullsail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Week 2/EventBook/EventBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EventBook
+//
+//  Created by Matthew Lewis on 10/6/14.
+//  Copyright (c) 2014 com.fullsail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
